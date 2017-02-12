eed76d2952325dc59e90b07d606afdeb4b1c29b0
Mateusz Ślażyński <mateusz.slazynski@gmail.com>
2017-02-12 01:04:00 +0100

    Created an Xcode project

---
+++ SwiftLox/SwiftLox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLox
+//
+//  Created by Mateusz Ślażyński on 12.02.2017.
+//  Copyright © 2017 Mateusz Ślażyński. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
